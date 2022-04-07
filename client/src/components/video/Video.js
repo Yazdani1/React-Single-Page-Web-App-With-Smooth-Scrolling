7f51e8ab05bdf7cb83aecf6cb268e763cb421c37
Yazdani <yaz4dani@gmail.com>
2022-04-07 21:23:13 +0300

    video features

--- client/src/components/video/Video.js
+++ client/src/components/video/Video.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React from "react";
 import "./video.css";
 
 const Video = () => {
   return (
     <React.Fragment>
-        <div className='container'>
-
+      <div className="container">
+        <div className="services-title">
+          <Flip left>
+            <h2>Services</h2>
+            <span className="line"></span>
+          </Flip>
         </div>
-    </React.Fragment>
-  )
-}
+        <div className="row">
+          <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+          <div className="card services">
+                  <h6>Video Demo</h6>
+                  <p>Video Description</p>
+                  <span className="view-details-button">View Details</span>
 
-export default Video
+                </div>
+          </div>
+
+          <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+              <div className="youtube-video">
+                  <iframe src=""></iframe>
+              </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Video;
