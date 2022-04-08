9e16ca8786c3fae6a3b8ba2a249371daa2b95c8b
Yazdani <yaz4dani@gmail.com>
2022-04-08 23:35:27 +0300

    get api data

--- client/src/components/Career/Career.js
+++ client/src/components/Career/Career.js
@@ -1,18 +1,30 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./career.css";
 
 const Career = () => {
+  const URL = "https://jsonplaceholder.typicode.com/posts";
 
-  const URL="https://jsonplaceholder.typicode.com/posts";
+  const [data, setData] = useState([]);
 
-  const [data,setData] = useState([]);
+  const loadData = () => {
+    fetch(URL, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result) {
+          setData(result);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-
-
-
+  useEffect(() => {}, []);
 
   return (
-    <div className="career" id='career'>
+    <div className="career" id="career">
       <h2>Career Page</h2>
     </div>
   );
