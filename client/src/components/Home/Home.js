8406e60308726b492244023af2219ad0d8b715e5
Yazdani <yaz4dani@gmail.com>
2022-04-09 16:00:15 +0300

    error solved

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,15 +1,27 @@
 import React from "react";
 import "./home.css";
+import Fade from "react-reveal/Fade";
+import Flip from "react-reveal/Flip";
+
 const Home = () => {
   return (
     <div className="container-fluid home" id="home">
-      <div className="container home-content">
-        <h4>24 Hours Service Center</h4>
-     
-        <p>Worlds Leading Company</p>
-        <p>Doesn't come with instructions</p>
-        <span className="view-details-home-button">View Details</span>
-      </div>
+      <Fade left>
+        <div className="container home-content">
+          <h1>24 Hours Service Center</h1>
+
+          <h4>Worlds Leading Company</h4>
+          <h4>Doesn't come with instructions</h4>
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English.
+          </p>
+          <span className="view-details-home-button">View Details</span>
+        </div>
+      </Fade>
     </div>
   );
 };
