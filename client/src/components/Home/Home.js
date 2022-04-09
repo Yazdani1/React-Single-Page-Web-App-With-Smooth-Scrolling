5df1b44a770bafe602bb5f8bff7b67ddc7f52d28
Yazdani <yaz4dani@gmail.com>
2022-04-09 15:49:54 +0300

    new features

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,12 +3,12 @@ import "./home.css";
 const Home = () => {
   return (
     <div className="container-fluid home" id="home">
-      <div className="home-content">
+      <div className="container home-content">
         <h4>24 Hours Service Center</h4>
      
         <p>Worlds Leading Company</p>
         <p>Doesn't come with instructions</p>
-        <span className="view-details-button">View Details</span>
+        <span className="view-details-home-button">View Details</span>
       </div>
     </div>
   );
