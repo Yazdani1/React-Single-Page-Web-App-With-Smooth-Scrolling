9c8f5355e2cc367047f8c8d212b0fe34b0429583
Yazdani <yaz4dani@gmail.com>
2022-04-13 20:42:56 +0300

    footer items

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,20 +1,16 @@
-import React from 'react'
+import React from "react";
 import "./footer.css";
 
 const Footer = () => {
   return (
     <React.Fragment>
-        <div className='container-fluid footer'>
-
-          <div className='row'>
-            <div>
-              
-            </div>
-          </div>
-
+      <div className="container-fluid footer">
+        <div className="row">
+          <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12"></div>
         </div>
+      </div>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
