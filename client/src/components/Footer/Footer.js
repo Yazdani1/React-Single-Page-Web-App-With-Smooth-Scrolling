6eddc82d011cd780eb782944ffdfcd28a548dd18
Yazdani <yaz4dani@gmail.com>
2022-04-14 21:03:55 +0300

    footer design

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,7 +6,15 @@ const Footer = () => {
     <React.Fragment>
       <div className="container-fluid footer">
         <div className="row">
-          <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12"></div>
+          <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+            <h6>Quick Links</h6>
+          </div>
+          <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+            <h6>More Services</h6>
+          </div>
+          <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+            <h6>Quick Links</h6>
+          </div>
         </div>
       </div>
     </React.Fragment>
