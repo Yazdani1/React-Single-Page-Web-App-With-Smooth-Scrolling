a58f96b41f02b063d53f13a754db2fca51231847
Yazdani <yaz4dani@gmail.com>
2022-04-19 22:31:02 +0300

    subscription section design

--- client/src/components/subscription/Subscription.js
+++ client/src/components/subscription/Subscription.js
@@ -3,7 +3,7 @@ import React from "react";
 const Subscription = () => {
   return (
     <React.Fragment>
-      <div className="subscription">
+      <div className="container-fluid subscription">
         <h5>Subscribe to the channel</h5>
       </div>
     </React.Fragment>
