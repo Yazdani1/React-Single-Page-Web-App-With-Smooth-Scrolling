cafe043db59a9cc102c10f1d0088f08ecbcadd88
Yazdani <yaz4dani@gmail.com>
2022-04-23 16:20:41 +0300

    Image Zoom In

--- client/src/components/Team/Team.js
+++ client/src/components/Team/Team.js
@@ -1,9 +1,60 @@
 import React from "react";
 import "./team.css";
 import Fade from "react-reveal/Fade";
-import Flip from 'react-reveal/Flip';
+import Flip from "react-reveal/Flip";
 
 const Team = () => {
+  const data = [
+    {
+      content:
+        "It is not every day that you come across a passionate and trustworthy financial advisor",
+      name: "Jon Doe",
+      position: "Full stack software developer",
+      img: "https://images.pexels.com/photos/2613260/pexels-photo-2613260.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      id: 1,
+    },
+    {
+      content:
+        "It is not every day that you come across a passionate and trustworthy financial advisor",
+      name: "Muller",
+      position: "UI/UX Designer",
+      img: "https://images.pexels.com/photos/2406949/pexels-photo-2406949.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      id: 2,
+    },
+    {
+      content:
+        "It is not every day that you come across a passionate and trustworthy financial advisor",
+      name: "Alexander",
+      position: "Javascript developer",
+      img: "https://images.pexels.com/photos/1542085/pexels-photo-1542085.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      id: 3,
+    },
+    {
+      content:
+        "It is not every day that you come across a passionate and trustworthy financial advisor",
+      name: "Bochum",
+      position: "Node js developer",
+      img: "https://images.pexels.com/photos/3608039/pexels-photo-3608039.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      id: 4,
+    },
+    {
+      content:
+        "It is not every day that you come across a passionate and trustworthy financial advisor",
+      name: "Bochum",
+      position: "Nuxt js developer",
+      img: "https://images.pexels.com/photos/3608039/pexels-photo-3608039.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      id: 5,
+    },
+    {
+      content:
+        "It is not every day that you come across a passionate and trustworthy financial advisor",
+      name: "Bochum",
+      position: "Nuxt js developer",
+      img: "https://images.pexels.com/photos/3608039/pexels-photo-3608039.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      id: 5,
+    },
+  ];
+
   return (
     <React.Fragment>
       <div className="container team-members" id="teammemebrs">
@@ -13,6 +64,23 @@ const Team = () => {
             <span className="line"></span>
           </Flip>
         </div>
+        <div className="row">
+          {data.map((item, index) => (
+            <>
+              <div className="col-lg-4 col-md-6 col-xl-4">
+                <div className="card team-members-item">
+                  <img src={item.img} />
+
+                  <div className="team-info">
+                    <h6>{item.name}</h6>
+                    <p>{item.content}</p>
+                  </div>
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+
       </div>
     </React.Fragment>
   );
