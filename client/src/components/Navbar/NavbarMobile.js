b5be965e3fb2ef255255a8769b9bcace8230b980
Yazdani <yaz4dani@gmail.com>
2022-04-24 15:37:55 +0300

    Navbar Mobile View

--- client/src/components/Navbar/NavbarMobile.js
+++ client/src/components/Navbar/NavbarMobile.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 import "./navmobile.css";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const NavbarMobile = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
     <React.Fragment>
-        <nav className='nav-mobile'>
-fssdf
-        </nav>
-    </React.Fragment>
-  )
-}
+      <nav className="nav-mobile">
+        <p onClick={handleClick}>
+          <GiHamburgerMenu size={30} />
+        </p>
 
-export default NavbarMobile
+    
+      </nav>
+      {open ? <div className="nav-item-list">
+          
+          </div> : null}
+    </React.Fragment>
+  );
+};
+
+export default NavbarMobile;
