2a13717345368a1d14b63fe30c770ad3e36c9960
Yazdani <yaz4dani@gmail.com>
2022-04-25 22:51:06 +0300

    Footer Design

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./footer.css";
 const Footer = () => {
   return (
     <React.Fragment>
-      <div className="container-fluid footer">
+      <div className="container footer">
         <div className="row">
           <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12">
             <div className="footer-quicklink-section">
