19b2a95a32ca1657b5374fe0fb6ead257e3ca74d
Yazdani <yaz4dani@gmail.com>
2022-04-25 22:52:09 +0300

    Color Design

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -16,6 +16,11 @@ const Footer = () => {
           </div>
           <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12">
             <h6>More Services</h6>
+            <div className="services">
+              <p>Home</p>
+              <p>About</p>
+              <p>Service</p>
+            </div>
           </div>
           <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12">
             <h6>Quick Links</h6>
