9b83ee8b622a6495fd8100485bb2f8273a52487f
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-05-23 20:25:13 +0300

    Adding Collapse option in the card

--- client/src/components/Career/Data.js
+++ client/src/components/Career/Data.js
@@ -8,17 +8,17 @@ import {
   FcApproval,
 } from "react-icons/fc";
 
-const Data = ({ title, body }) => {
+const Data = ({ title, body, id }) => {
   //collapse and show each item features
 
   const [show, setShow] = useState(false);
 
   return (
-    <div className={show ? "post-card-open" : "post-card"}>
-      <div
-        onClick={() => setShow(!show)}
-        className={show ? "collapse-option item-title" : "collapse-option"}
-      >
+    <div
+      onClick={() => setShow(!show)}
+      className={show ? "post-card-open" : "post-card"}
+    >
+      <div className={show ? "collapse-option item-title" : "collapse-option"}>
         <h6>{title}</h6>
         <p>{show ? <FcCollapse size={20} /> : <FcExpand size={20} />}</p>
       </div>
