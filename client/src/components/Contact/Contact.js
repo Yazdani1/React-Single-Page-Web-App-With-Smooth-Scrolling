81adbeada3497f691aa81276375acc22547877dd
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-06-14 20:47:53 +0300

    Designing Button

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -2,69 +2,70 @@ import React from "react";
 import "./contact.css";
 import Fade from "react-reveal/Fade";
 import Flip from "react-reveal/Flip";
+import { RiSendPlaneFill } from "react-icons/ri";
 
 const Contact = () => {
   return (
     <React.Fragment>
       <div className="container-fluid contact-section" id="contact">
-          <div className="row">
-            <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4">
-              <div style={{height:"70vh",background:"red"}}>
+        <div className="row">
+          <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4">
+            <div style={{ height: "70vh", background: "red" }}></div>
+          </div>
+          <div className="col-xl-8 col-lg-8 col-md-8 col-sm-8">
+            <div className="card event-form-design">
+              <Flip left>
+                <div className="text-center">
+                  <h5 className="text-center">Contact Form</h5>
+                </div>
+              </Flip>
+              <form>
+                <div className="event-form">
+                  <label for="exampleInputEmail1" className="form-label">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    maxLength="100"
+                    placeholder="Your name..."
+                  />
+                </div>
+                <div className="event-form">
+                  <label for="exampleInputEmail1" className="form-label">
+                    E-mail
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    maxLength="100"
+                    placeholder="Your e-mail..."
+                  />
+                </div>
 
-              </div>
-            </div>
-            <div className="col-xl-8 col-lg-8 col-md-8 col-sm-8">
-              <div className="card event-form-design">
-                <Flip left>
-                  <div className="text-center">
-                    <h5 className="text-center">Contact Form</h5>
-                  </div>
-                </Flip>
-                <form>
-                  <div className="event-form">
-                    <label for="exampleInputEmail1" className="form-label">
-                      Name
-                    </label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      maxLength="100"
-                      placeholder="Your name..."
-                    />
-                  </div>
-                  <div className="event-form">
-                    <label for="exampleInputEmail1" className="form-label">
-                      E-mail
-                    </label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      maxLength="100"
-                      placeholder="Your e-mail..."
-                    />
-                  </div>
+                <div className="event-form">
+                  <label for="exampleInputEmail1" className="form-label">
+                    Your Message
+                  </label>
+                  <textarea
+                    type="text"
+                    className="form-control"
+                    maxLength="100"
+                    rows="4"
+                    placeholder="Your message..."
+                  />
+                </div>
 
-                  <div className="event-form">
-                    <label for="exampleInputEmail1" className="form-label">
-                      Your Message
-                    </label>
-                    <textarea
-                      type="text"
-                      className="form-control"
-                      maxLength="100"
-                      rows="4"
-                      placeholder="Your message..."
-                    />
-                  </div>
-
-                  <div class="submit-button">
-                    <p>Send</p>
-                  </div>
-                </form>
-              </div>
+                <div class="submit-button">
+                  <p>
+                    Send <RiSendPlaneFill size={20} />
+                  </p>
+                </div>
+              </form>
             </div>
           </div>
         </div>
+      </div>
     </React.Fragment>
   );
 };
