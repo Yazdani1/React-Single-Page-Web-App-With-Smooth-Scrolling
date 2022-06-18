a5ee1c2b742098588bee60e68c7b3a12c62e7125
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-06-18 14:27:23 +0300

    Design changes

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -8,28 +8,25 @@ const Contact = () => {
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [message, setMessage] = useState();
+  const [serviceType, setServiceType] = useState();
 
   return (
     <React.Fragment>
       <div className="container contact-section" id="contact">
         <div className="row">
           <div className="col-xl-5 col-lg-5 col-md-5 col-sm-5">
-              <div className="image-class">
-               
-              </div>
-   
+            <div className="image-class"></div>
           </div>
 
           <div className="col-xl-7 col-lg-7 col-md-7 col-sm-7">
-            
-            <div className="event-form-design">
+            <div className="contact-form-design">
               <Flip left>
                 <div className="text-center">
                   <h5 className="text-center">Contact Form</h5>
                 </div>
               </Flip>
               <form>
-                <div className="event-form">
+                <div className="contact-form">
                   <label for="exampleInputEmail1" className="form-label">
                     Name
                   </label>
@@ -42,7 +39,7 @@ const Contact = () => {
                     placeholder="Your name..."
                   />
                 </div>
-                <div className="event-form">
+                <div className="contact-form">
                   <label for="exampleInputEmail1" className="form-label">
                     E-mail
                   </label>
@@ -55,22 +52,23 @@ const Contact = () => {
                     placeholder="Your e-mail..."
                   />
                 </div>
-                <div className="event-form">
+                <div className="contact-form">
                   <label for="exampleInputEmail1" className="form-label">
-                    Job types
+                    Service you need
                   </label>
                   <select
                     className="custom-select"
-                 
+                    value={serviceType}
+                    onChange={(e) => setServiceType(e.target.value)}
                   >
-                    <option value="Full-Time">Full-Time</option>
-                    <option value="Part-Time">Part-Time</option>
-                    <option value="For Students">For Students</option>
-                    <option value="Contract">Contract</option>
+                    <option value="Web Development">Web Development</option>
+                    <option value="Mobile App">Mobile App</option>
+                    <option value="UI/UX Design">UI/UX Design</option>
+                    <option value="Digital Marketing">Digital Marketing</option>
                   </select>
                 </div>
 
-                <div className="event-form">
+                <div className="contact-form">
                   <label for="exampleInputEmail1" className="form-label">
                     Your Message
                   </label>
@@ -92,8 +90,6 @@ const Contact = () => {
                 </div>
               </form>
             </div>
-
-
           </div>
         </div>
       </div>
