1666ff1d641ebf6f9775fb4be2acf042070aa343
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-06-19 19:22:07 +0300

    make it better

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 const sendgridTransport = require("nodemailer-sendgrid-transport");
-const SendEmail = require("./model/SendEmail");
+// const SendEmail = require("./model/SendEmail");
 require("dotenv").config();
 
 // "proxy": "http://localhost:8080"
 // "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix client && npm run build --prefix client"
 
 //db connection
-require("./model/db");
+// require("./model/db");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -80,6 +80,21 @@ app.post("/sendemail", (req, res) => {
 
   res.json({ name });
 });
+
+
+
+//to deploy heroku
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 
 app.listen(PORT, (req, res) => {
   console.log("Server connected");
