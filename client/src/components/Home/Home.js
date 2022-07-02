be26f309381b41b0c8d4d5322cb96001fecec2e1
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-07-02 22:48:26 +0300

    Code formated

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -10,8 +10,6 @@ import {
 import { BsFillSunFill, BsFillMoonStarsFill } from "react-icons/bs";
 import Typewriter from "typewriter-effect";
 
-// "fs": "^0.0.1-security",
-
 const Home = ({ toggleTheme, theme }) => {
   const [show, setShow] = useState(false);
 
@@ -20,47 +18,47 @@ const Home = ({ toggleTheme, theme }) => {
   };
 
   return (
-      <div className="container-fluid home" id="home">
-        <div className="theme-chang " onClick={() => toggleTheme()}>
-          {theme === "light" ? (
-            <p className="theme-iconss">
-              <BsFillMoonStarsFill size={40} />
-            </p>
-          ) : (
-            <p className="theme-icon">
-              <BsFillSunFill size={40} />
-            </p>
-          )}
+    <div className="container-fluid home" id="home">
+      <div className="theme-chang " onClick={() => toggleTheme()}>
+        {theme === "light" ? (
+          <p className="theme-iconss">
+            <BsFillMoonStarsFill size={40} />
+          </p>
+        ) : (
+          <p className="theme-icon">
+            <BsFillSunFill size={40} />
+          </p>
+        )}
 
-          {/* <p className="theme-icon">
+        {/* <p className="theme-icon">
           <AiFillSetting size={40} />
         </p> */}
-          {/* <p style={{ top: 0 }}>
+        {/* <p style={{ top: 0 }}>
           {theme === "light" ? "Dark Theme" : "Light Theme"}
         </p> */}
-        </div>
+      </div>
 
-        <Fade left>
-          <div className="container home-content">
-            <h1>
-              <Typewriter
-                options={{
-                  strings: [
-                    "Web Development",
-                    "Mobile App Development",
-                    "UI/UX Design",
-                    "Product Marketing",
-                    "Digital Marketing",
-                    "Customer Service",
-                  ],
-                  autoStart: true,
-                  loop: true,
-                  delay: 10,
-                }}
-              />
-            </h1>
-
-            {/* <Typical
+      <Fade left>
+        <div className="container home-content">
+         
+          <h1>
+            <Typewriter
+              options={{
+                strings: [
+                  "Web Development",
+                  "Mobile App Development",
+                  "UI/UX Design",
+                  "Product Marketing",
+                  "Digital Marketing",
+                  "Customer Service",
+                ],
+                autoStart: true,
+                loop: true,
+                delay: 10,
+              }}
+            />
+          </h1>
+          {/* <Typical
             steps={[
               "Web Development",
               4000,
@@ -78,21 +76,19 @@ const Home = ({ toggleTheme, theme }) => {
             loop={Infinity}
             wrapper="h1"
           /> */}
-            {/* <h4>Worlds Leading Company</h4> */}
-            {/* <h6>Doesn't come with instructions</h6> */}
-            <p>
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using 'Content here,
-              content here', making it look like readable English.
-            </p>
-            <span className="view-details-home-button">View Details</span>
-          </div>
-        </Fade>
-
-      </div>
-   
+          {/* <h4>Worlds Leading Company</h4> */}
+          {/* <h6>Doesn't come with instructions</h6> */}
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English.
+          </p>
+          <span className="view-details-home-button">View Details</span>
+        </div>
+      </Fade>
+    </div>
   );
 };
 
